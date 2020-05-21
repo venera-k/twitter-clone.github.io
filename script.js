8b37b34ec248eb1a92ed16dab73077c6dc09890b
Venera <64845033+venera-k@users.noreply.github.com>
2020-05-21 18:16:05 +0600

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,19 @@ const newsFeedPage = document.querySelector('.feeds-page');
 const loginModal = document.querySelector('.login-modal');
 const modalX = document.querySelector('.login-modal i');
 const loginFormBtn = document.querySelector('.login-form-btn');
+const postBtn = document.querySelector('.post-btn');
+const modalWrapper = document.querySelector('.modal-wrapper');
+const modal = document.querySelector('.modal');
+const postModalX = document.querySelector('.modal-header i');
+const modalPostBtn = document.querySelector('.modal-header button');
+const modalFooterPlus = document.querySelector('.modal-footer span');
+const modalInput = document.querySelector('.modal-input');
+const user = document.querySelector('.user');
+const sidebar = document.querySelector('.sidebar');
+const sidebarWrapper = document.querySelector('.sidebar-wrapper');
+const xBtn = document.querySelector('.sidebar-header i');
+const toggle = document.querySelector('.toggle');
+const circle = document.querySelector('.circle');
 
 /*********************************************************/
 /*********************************************************/
@@ -41,13 +54,13 @@ btnTop.addEventListener('click', () => {
         goToLoginPage();
         loginModal.style.display = 'block';
     }
-})
+});
 
 // Login Page
 
 modalX.addEventListener('click', () => {
     loginModal.style.display = 'none';
-})
+});
 
 loginFormBtn.addEventListener('click', () => {
     const loginUserInfo = document.querySelector('.login-user-info');
@@ -60,4 +73,72 @@ loginFormBtn.addEventListener('click', () => {
     else {
         loginModal.style.display = 'block';
     }
-})
+});
+
+// News feed page
+// Post modal
+
+postBtn.addEventListener('click', () =>
+{
+    modal.style.display = 'block';
+    modalWrapper.classList.add('modal-wrapper-display');
+});
+
+const changeOpacity = (x) => {
+    modalPostBtn.style.opacity = x;
+    modalFooterPlus.style.opacity = x; 
+
+}
+
+postModalX.addEventListener('click', () =>
+   {
+       modal.style.display = 'none';
+       modalWrapper.classList.remove('modal-wrapper-display');
+
+       if(modalInput.value !== "") {
+           modalInput.value = "";
+           changeOpacity(0.5);
+       }
+   } 
+);
+
+modalInput.addEventListener('keypress', (e) => 
+{
+    if(e.target.value !== "") {
+        changeOpacity(1);
+    }
+});
+
+modalInput.addEventListener('blur', (e) => {
+    if(e.target.value === '') {
+        changeOpacity(0.5);
+    }
+});
+
+// sidebar
+
+user.addEventListener('click', () => 
+{
+    sidebar.classList.add('sidebar-display');
+    sidebarWrapper.classList.add('sidebar-wrapper-display');
+});
+
+xBtn.addEventListener('click', () => {
+    sidebar.classList.remove('sidebar-display');
+    sidebarWrapper.classList.remove('sidebar-wrapper-display');
+});
+
+// Dark mode
+
+const darkElements1 = document.querySelectorAll('.dark-mode-1');
+const darkElements2 = document.querySelectorAll('.dark-mode-2');
+const lightTexts = document.querySelectorAll('.light-text');
+const borders = document.querySelectorAll('.border-color');
+
+toggle.addEventListener('click', () => {
+    circle.classList.toggle('move');
+    Array.from(darkElements1).map((darkEl1) => darkEl1.classList.toggle('dark-1'));
+    Array.from(darkElements2).map((darkEl2) => darkEl2.classList.toggle('dark-2'));
+    Array.from(lightTexts).map((lightText) => lightText.classList.toggle('light'));
+    Array.from(borders).map((border) => border.classList.toggle('border-color'));
+});
